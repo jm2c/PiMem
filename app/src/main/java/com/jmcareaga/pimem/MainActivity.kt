5d6de6d83852683c3df81ded4c840748758b7e57
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2023-03-13 15:14:18 -0600

    Funciona la lógica (ver Logs)

--- app/src/main/java/com/jmcareaga/pimem/MainActivity.kt
+++ app/src/main/java/com/jmcareaga/pimem/MainActivity.kt
@@ -2,17 +2,86 @@ package com.jmcareaga.pimem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var game: MemGame
+    private lateinit var btn0: Button
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
+    private lateinit var btn4: Button
+    private lateinit var btn5: Button
+    private lateinit var btn6: Button
+    private lateinit var btn7: Button
+    private lateinit var btn8: Button
+    private lateinit var btn9: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         game = MemGame()
+        btn0 = findViewById(R.id.btn0)
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn4 = findViewById(R.id.btn4)
+        btn5 = findViewById(R.id.btn5)
+        btn6 = findViewById(R.id.btn6)
+        btn7 = findViewById(R.id.btn7)
+        btn8 = findViewById(R.id.btn8)
+        btn9 = findViewById(R.id.btn9)
 
-        for(i in 0..10) {
-            println(game.nextDigit())
+
+        btn0.setOnClickListener{
+            game.step(0)
+            checkGameStatus()
+        }
+        btn1.setOnClickListener{
+            game.step(1)
+            checkGameStatus()
+        }
+        btn2.setOnClickListener{
+            game.step(2)
+            checkGameStatus()
+        }
+        btn3.setOnClickListener{
+            game.step(3)
+            checkGameStatus()
+        }
+        btn4.setOnClickListener{
+            game.step(4)
+            checkGameStatus()
+        }
+        btn5.setOnClickListener{
+            game.step(5)
+            checkGameStatus()
+        }
+        btn6.setOnClickListener{
+            game.step(6)
+            checkGameStatus()
+        }
+        btn7.setOnClickListener{
+            game.step(7)
+            checkGameStatus()
+        }
+        btn8.setOnClickListener{
+            game.step(8)
+            checkGameStatus()
+        }
+        btn9.setOnClickListener{
+            game.step(9)
+            checkGameStatus()
+        }
+    }
+
+    private fun checkGameStatus(){
+        if (game.isFinished()) {
+            val score = game.getScore()
+            Log.d("Game", "The Game has been terminated: $score")
+        } else {
+            Log.d("Game", "Continue...")
         }
     }
 }
--- app/src/main/java/com/jmcareaga/pimem/MemGame.kt
+++ app/src/main/java/com/jmcareaga/pimem/MemGame.kt
@@ -1,12 +1,34 @@
 package com.jmcareaga.pimem
 
+import android.util.Log
+
 class MemGame {
+    private var finished: Boolean = false
     private var piStr: String = "3."
     private val digitsStr: String = "14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436"
     private val digits: CharArray = digitsStr.toCharArray()
     private var index: Int = 0
 
-    fun nextDigit(): Int {
+    private fun nextDigit(): Int {
         return digits[index++].digitToInt()
+    }
+
+    fun step(guess: Int): Boolean {
+        if(finished)
+            return false
+
+        Log.d("Game", "Your guess: $guess")
+        val isCorrect = (guess == nextDigit().toInt())
+        if(!isCorrect)
+            finished = !finished
+        return isCorrect
+    }
+
+    fun getScore(): Int {
+        return index
+    }
+
+    fun isFinished(): Boolean {
+        return finished
     }
 }
